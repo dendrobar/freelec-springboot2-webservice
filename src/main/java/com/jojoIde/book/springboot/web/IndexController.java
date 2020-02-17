c1dfbbf3724603c1b359a7224906122c6f9d9342
Mr.Lee <dendrobar@gmail.com>
2020-02-17 14:54:50 +0900

    로그인시 유저정보 어노테이션으로 관리

--- src/main/java/com/jojoIde/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoIde/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoIde.book.springboot.web;
 
+import com.jojoIde.book.springboot.config.auth.LoginUser;
 import com.jojoIde.book.springboot.config.auth.dto.SessionUser;
 import com.jojoIde.book.springboot.service.PostsService;
 import com.jojoIde.book.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping(value = "/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
